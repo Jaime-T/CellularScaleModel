4e002d6b70585feaa3fb46f8b0cdceb03126b5c5
Jaime Taitz <jtaitz@CCILTMAC5002.local>
2025-06-05 11:32:52 +1000

    aadded new comments

--- mutant_data.py
+++ mutant_data.py
@@ -3,16 +3,17 @@
  
 import pandas as pd
 import numpy as np
-from getSequence import getseq
+from getSequence import getseq # reference: https://github.com/ryanemenecker/getSequence 
 
 def get_wildtype_sequence(uniprot_id):
     return getseq(str(uniprot_id), uniprot_id=True, just_sequence=True)
  
 def main():
- 
+
+    # load depmap data from local file
     depmap_df = pd.read_csv('/Users/jtaitz/Documents/Honours/datasets/depmap/OmicsSomaticMutations.csv', sep=',', low_memory=False)
     
-    # Identify which samples have an associated UniProtID in the UniprotID column (many are empty, with no UniprotID)
+    # Identify which samples have an associated UniprotID in the UniprotID column (many are empty, with no UniprotID)
     depmap_with_uniprotid = depmap_df[depmap_df['UniprotID'].notna()] # 407669 out of 718369 have uniprotID not null (uniprot_ids.count())
     
     # Extract the UniprotID from those samples, and print them
@@ -24,9 +25,9 @@ def main():
     # Get the protein sequence from the sample's UniprotID using the getseq function from the getSequence package
     # Example is: seq = getseq(['Q9BY66-1', 'Q9BY66-1'], uniprot_id=True, just_sequence=True)
 
-    # Make a new column for wildtype protein sequence, and mutant type sequence
-    depmap_with_uniprotid['wildtype_seq'] = np.nan
-    depmap_with_uniprotid['mutant_seq'] = np.nan
+    # Make a new empty column for wildtype protein sequence, and mutant type sequence
+    depmap_with_uniprotid['wildtype_seq'] = ''
+    depmap_with_uniprotid['mutant_seq'] = ''
 
     # Add wildtype sequence to each row .. need to fix 
     depmap_with_uniprotid['wildtype_seq'] = depmap_with_uniprotid['UniprotID'].apply(get_wildtype_sequence)
