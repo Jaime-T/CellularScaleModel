4c97900d428b899d04d57d9bfcb802a00560a399
Jaime <z5359312@ad.unsw.edu.au>
2025-06-18 16:39:45 +1000

    added filtering function

--- process_data.py
+++ process_data.py
@@ -11,7 +11,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-def analyse(df):
+def analyse(df: object):
     
     # Filter to non-null mt_protein_seq
     df = df[df['mt_protein_seq'].notnull()].copy()
@@ -51,20 +51,58 @@ def analyse(df):
     plt.grid(True, linestyle='--', alpha=0.5)
     plt.xlim(0, stats['Maximum'] + bin_width)
     plt.tight_layout()
-    plt.show()
+    #plt.show()
+
+    # Unique proteins 
+    print(f"\nNumber of unique proteins by Hugo Symbol: {len(df['HugoSymbol'].unique())}")
+    hugo_count = df['HugoSymbol'].value_counts()
+    top = 100
+    print(f"\nTop {top}: {hugo_count.head(top)}\n")
+
+def filter(df: object, num: int):
+    """ Filter out sequences that no protein sequence,  
+        and filter out sequences in genes that have 
+        less than a specified number of variants 
+    """
+    # Filter out null mt_protein_seq
+    df = df[df['mt_protein_seq'].notnull()].copy()
+
+    # Filter out low variant genes
+    counts = df['HugoSymbol'].value_counts()
+    valid = counts[counts > num].index
+    df = df[df['HugoSymbol'].isin(valid)]
+    return df
 
 
+def filter_var(df: object, var: str):
+    """Variant type can be: SNV, substitution, insertion, deletion
+    """
+
+    # Filter those with variant type
+    snv = df[df['VariantType'] == str(var)]
+    return snv
 
 def main():
-    csv_path = '/Users/jaimetaitz/Downloads/OmicsSomaticMutations_with_protein_seqs.csv'
+
     # Load data 
+    csv_path = '/Users/jaimetaitz/Downloads/OmicsSomaticMutations_with_protein_seqs.csv'
     if not os.path.exists(csv_path):
         raise FileNotFoundError(f"File not found: {csv_path}")
-
     df = pd.read_csv(csv_path, low_memory=False)
     print(f"\nLoaded {len(df):,} records from {csv_path}")
 
+    # Data Analysis 
     analyse(df)
+
+    # Data Filtering
+    df = filter(df, 10)
+    print(df.shape)
+
+    # Filter df by the variant type  
+    snv_df = filter_var(df, 'SNV')
+    del_df = filter_var(df, 'deletion')
+    sub_df = filter_var(df, 'substitution')
+    ins_df = filter_var(df, 'insertion')
 
 
 
