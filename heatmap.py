f79d25b8f63a347a943feca2df7f7d73e19b28d4
Jaime Taitz <jtaitz@ccia.org.au>
2025-07-14 00:54:28 +0000

    cleaned up

--- heatmap.py
+++ heatmap.py
@@ -10,15 +10,15 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from transformers import AutoTokenizer, EsmModel, AutoModelForMaskedLM
-from transformers import EsmForMaskedLM, EsmTokenizer 
-
+from transformers import EsmForMaskedLM, EsmTokenizer, AutoModel
 
 def generate_heatmap(protein_sequence, model, tokenizer, start_pos=1, end_pos=None):
     model.eval()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    input_ids = tokenizer.encode(protein_sequence, return_tensors="pt").to(device)
+    decoded = tokenizer(protein_sequence, return_tensors="pt").to(device)
+    input_ids = decoded['input_ids']
     sequence_length = input_ids.shape[1] - 2
 
     if end_pos is None:
@@ -33,9 +33,8 @@ def generate_heatmap(protein_sequence, model, tokenizer, start_pos=1, end_pos=No
 
         with torch.no_grad():
             logits = model(masked_input_ids).logits
-
-        probabilities = torch.nn.functional.softmax(logits[0, position], dim=0)
-        log_probabilities = torch.log(probabilities)
+            probabilities = torch.nn.functional.softmax(logits[0, position], dim=0)
+            log_probabilities = torch.log(probabilities)
 
         wt_residue = input_ids[0, position].item()
         log_prob_wt = log_probabilities[wt_residue].item()
@@ -47,9 +46,9 @@ def generate_heatmap(protein_sequence, model, tokenizer, start_pos=1, end_pos=No
 
     return heatmap, amino_acids
 
-def plot_heatmap(data, title, sequence, amino_acids, start_pos=1):
-    plt.figure(figsize=(15, 5))
-    plt.imshow(data, cmap="bwr_r" if "Difference" in title else "viridis", aspect="auto", vmin=None, vmax=None)
+def plot_heatmap(gene, data, title, sequence, amino_acids, start_pos=1):
+    plt.figure(figsize=(20, 5))
+    plt.imshow(data, cmap="bwr_r" if "Difference" in title else "viridis_r", aspect="auto", vmin=None, vmax=None)
     plt.xticks(range(len(sequence)), list(sequence))
     plt.yticks(range(20), amino_acids)
     plt.xlabel("Position in Protein Sequence")
@@ -58,7 +57,7 @@ def plot_heatmap(data, title, sequence, amino_acids, start_pos=1):
     plt.colorbar(label="Log Likelihood Ratio (LLR)")
     plt.tight_layout()
     
-    plt.savefig(f"heatmaps/missense_tst/{title.replace(' ', '_')}.png", dpi=300)
+    plt.savefig(f"heatmaps/{gene}/{title.replace(' ', '_')}.png", dpi=300) 
 
 def topk_predictions(model, tokenizer, protein_seq, masked_pos, k=5):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -86,27 +85,30 @@ def topk_predictions(model, tokenizer, protein_seq, masked_pos, k=5):
 
 def main():
 
+    # Load original ESM-2 model
+    #base_model_name = "facebook/esm2_t33_650M_UR50D"
+    base_model_name = "facebook/esm2_t6_8M_UR50D"
+    base_tokenizer = EsmTokenizer.from_pretrained(base_model_name)
+    base_model = EsmForMaskedLM.from_pretrained(base_model_name)
+    #base_model, base_tokenizer = esm.pretrained.esm2_t33_650M_UR50D()
+
     # Load missense fine-tuned model
-    ms_model_path = "./train/model_missense"
+    ms_model_path = "./train_8M/model_missense"
     ms_tokenizer = EsmTokenizer.from_pretrained(ms_model_path)
     ms_model = EsmForMaskedLM.from_pretrained(ms_model_path)
 
     # Load frameshift fine-tuned model
-    fs_model_path = "./train/model_frameshift"
+    fs_model_path = "./train_8M/model_frameshift"
     fs_tokenizer = EsmTokenizer.from_pretrained(fs_model_path)
     fs_model = EsmForMaskedLM.from_pretrained(fs_model_path)
 
-    # Load original ESM-2 model
-    base_model_name = "facebook/esm2_t6_8M_UR50D"
-    base_tokenizer = EsmTokenizer.from_pretrained(base_model_name)
-    base_model = EsmForMaskedLM.from_pretrained(base_model_name)
+    # myc seq
+    sequence = "MDFFRVVENQQPPATMPLNVSFTNRNYDLDYDSVQPYFYCDEEENFYQQQQQSELQPPAPSEDIWKKFELLPTPPLSPSRRSGLCSPSYVAVTPFSLRGDNDGGGGSFSTADQLEMVTELLGGDMVNQSFICDPDDETFIKNIIIQDCMWSGFSAAAKLVSEKLASYQAARKDSGSPNPARGHSVCSTSSLYLQDLSAAASECIDPSVVFPYPLNDSSSPKSCASQDSSAFSPSSDSLLSSTESSPQGSPEPLVLHEETPPTTSSDSEEEQEDEEEIDVVSVEKRQAPGKRSESGSPSAGGHSKPPHSPLVLKRCHVSTHQHNYAAPPSTRKDYPAAKRVKLDSVRVLRQISNNRKCTSPRSSDTEENVKRRTHNVLERQRRNELKRSFFALRDQIPELENNEKAPKVVILKKATAYILSVQAEEQKLISEEDLLRKRREQLKHKLEQLRNSCA"
+    # tp53 seq
+    sequence = "MEEPQSDPSVEPPLSQETFSDLWKLLPENNVLSPLPSQAMDDLMLSPDDIEQWFTEDPGPDEAPRMPEAAPPVAPAPAAPTPAAPAPAPSWPLSSSVPSQKTYQGSYGFRLGFLHSGTAKSVTCTYSPALNKMFCQLAKTCPVQLWVDSTPPPGTRVRAMAIYKQSQHMTEVVRRCPHHERCSDSDGLAPPQHLIRVEGNLRVEYLDDRNTFRHSVVVPYEPPEVGSDCTTIHYNYMCNSSCMGGMNRRPILTIITLEDSSGNLLGRNSFEVRVCACPGRDRRTEEENLRKKGEPHHELPPGSTKRALPNNTSSSPQPKKKPLDGEYFTLQIRGRERFEMFRELNEALELKDAQAGKEPGGSRAHSSHLKSKKGQSTSRHKKLMFKTEGPDSD"
+    gene = 'tp53'
 
-    #sequence = "MKTAYIAKQRQISFVKSHFSRQLEERLGLIEVQ" random
-    #sequence = "QQSELQPPAPSEDIWKKFELLPTPPLSPSRRSGLCSPSYVAVTPFSLRGD" 51-100 
-    #sequence = "MDFFRVVENQQPPATMPLNVSFTNRNYDLDYDSVQPYFYCDEEENFYQQQQQSELQPPAPSEDIWKKFELLPTPPLSPSRRSGLCSPSYVAVTPFSLRGDNDGGGGSFSTADQLEMVTELLGGDMVNQSFICDPDDETFIKNIIIQDCMWSGFSAAAKLVSEKLASYQAARKDSGSPNPARGHSVCSTSSLYLQDLSAAASECIDPSVVFPYPLNDSSSPKSCASQDSSAFSPSSDSLLSSTESSPQGSPEPLVLHEETPPTTSSDSEEEQEDEEEIDVVSVEKRQAPGKRSESGSPSAGGHSKPPHSPLVLKRCHVSTHQHNYAAPPSTRKDYPAAKRVKLDSVRVLRQISNNRKCTSPRSSDTEENVKRRTHNVLERQRRNELKRSFFALRDQIPELENNEKAPKVVILKKATAYILSVQAEEQKLISEEDLLRKRREQLKHKLEQLR" # myc
-    sequence = "MPAVKKEFPGREDLALALATFHPTLAALPLPPLPGYLAPLPAAAALPPAASLPASAAGYEALLAPPLRPPRAYLSLHEAAPHLHLPRDPLALERFSATAAAAPDFQPLLDNGEPCIEVECGANRALLYVRKLCQGSKGPSIRHRGEWLTPNEFQFVSGRETAKDWKRSIRHKGKSLKTLMSKGILQVHPPICDCPGCRISSPVNRGRLADKRTVALPAARNLKKERTPSFSASDGDSDGSGPTCGRRPGLKQEDGPHIRIMKRRVHTHWDVNISFREASCSQDGNLPTLISSVHRSRHLVMPEHQSRCEFQRGSLEIGLRPAGDLLGKRLGRSPRISSDCFSEKRARSESPQEALLLPRELGPSMAPEDHYRRLVSALSEASTFEDPQRLYHLGLPSHDLLRVRQEVAAAALRGPSGLEAHLPSSTAGQRRKQGLAQHREGAAPAAAPSFSERELPQPPPLLSPQNAPHVALGPHLRPPFLGVPSALCQTPGYGFLPPAQAEMFAWQQELLRKQNLARLELPADLLRQKELESARPQLLAPETALRPNDGAEELQRRGALLVLNHGAAPLLALPPQGPPGSGPPTPSRDSARRAPRKGGPGPASARPSESKEMTGARLWAQDGSEDEPPKDSDGEDPETAAVGCRGPTPGQAPAGGAGAEGKGLFPGSTLPLGFPYAVSPYFHTGAVGGLSMDGEEAPAPEDVTKWTVDDVCSFVGGLSGCGEYTRVFREQGIDGETLPLLTEEHLLTNMGLKLGPALKIRAQVARRLGRVFYVASFPVALPLQPPTLRAPERELGTGEQPLSPTTATSPYGGGHALAGQTSPKQENGTLALLPGAPDPSQPLC"
-
-    # Generate
+    # Generate heatmaps
     base_heatmap, amino_acids = generate_heatmap(sequence, base_model, base_tokenizer)
     ms_heatmap, _ = generate_heatmap(sequence, ms_model, ms_tokenizer)
     fs_heatmap, _ = generate_heatmap(sequence, fs_model, fs_tokenizer)
@@ -115,11 +117,11 @@ def main():
     ms_diff_heatmap = ms_heatmap - base_heatmap
     fs_diff_heatmap = fs_heatmap - base_heatmap
 
-    plot_heatmap(base_heatmap, "Original ESM2 Model (LLRs)", sequence, amino_acids)
-    plot_heatmap(ms_heatmap, "Fine-tuned Missense Model (LLRs)", sequence, amino_acids)
-    plot_heatmap(fs_heatmap, "Fine-tuned Frameshift Model (LLRs)", sequence, amino_acids)
-    plot_heatmap(ms_diff_heatmap, "Difference (Fine-tuned Missense - Original)", sequence, amino_acids)
-    plot_heatmap(fs_diff_heatmap, "Difference (Fine-tuned Frameshift - Original)", sequence, amino_acids)
+    plot_heatmap(gene, base_heatmap, "Original ESM2 Model (LLRs)", sequence, amino_acids)
+    plot_heatmap(gene, ms_heatmap, "Fine-tuned Missense Model (LLRs)", sequence, amino_acids)
+    plot_heatmap(gene, fs_heatmap, "Fine-tuned Frameshift Model (LLRs)", sequence, amino_acids)
+    plot_heatmap(gene, ms_diff_heatmap, "Difference (Fine-tuned Missense - Original)", sequence, amino_acids)
+    plot_heatmap(gene, fs_diff_heatmap, "Difference (Fine-tuned Frameshift - Original)", sequence, amino_acids)
 
     # Comapre amino acid predictions
     masked_pos = 27
