8d7a2bfa29857321ef37e71893a79979e4c112a8
Jaime Taitz <jtaitz@ccia.org.au>
2025-10-21 00:34:48 +0000

    make heatmaps for first 5000 batches for tp53 for run10

--- gadi/heatmap_scaled_test.py
+++ gadi/heatmap_scaled_test.py
@@ -24,7 +24,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset, Subset 
 from transformers import EsmForMaskedLM, EsmTokenizer
-from sklearn.model_selection import train_test_split
+from peft import PeftModel
 
 class TorchDataset(Dataset):
     def __init__(self, data):
@@ -80,8 +80,38 @@ def generate_heatmap(protein_sequence, model, tokenizer, start_pos=1, end_pos=No
 
     return heatmap, amino_acids
 
-
 def plot_heatmap(data, gene, title, sequence, base_dir, amino_acids):
+
+    plt.figure(figsize=(20, 5))
+    plt.imshow(data, cmap="bwr_r" if "Difference" in title else "viridis_r", aspect="auto")
+    plt.yticks(range(20), amino_acids)
+    plt.ylabel("Amino Acid Mutations")
+
+    seq_len = len(sequence)
+    xticks_positions = list(range(0, seq_len, 50)) # mark every 50th position
+    # ensure last position is shown too
+    if seq_len - 1 not in xticks_positions:
+        xticks_positions.append(seq_len - 1)
+    # set ticks and labels
+    plt.xticks(xticks_positions, [str(pos) for pos in xticks_positions])
+    plt.xlabel("Position in Protein Sequence")
+    plt.title(title + ' ' + '650M')
+    plt.colorbar(label="Log Likelihood Ratio (LLR)")
+    plt.tight_layout()
+    
+    # Define the path
+    save_path = os.path.join(base_dir, f"{gene}/{title.replace(' ', '_')}.png")
+    folder = os.path.dirname(save_path)
+
+    # Create the directory if it doesn't exist
+    print(f"Saving heatmap to {save_path}")
+    os.makedirs(folder, exist_ok=True)
+
+    # Save the figure
+    plt.savefig(save_path, dpi=300)
+    plt.close()
+
+def scaled_plot_heatmap(data, gene, title, sequence, base_dir, amino_acids):
 
     # scaling data to -1 to 1
     data_min = np.min(data)
@@ -118,6 +148,7 @@ def plot_heatmap(data, gene, title, sequence, base_dir, amino_acids):
 
     # Save the figure
     plt.savefig(save_path, dpi=300)
+    plt.close()
 
 def topk_predictions(model, tokenizer, protein_seq, masked_pos, k=5):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -156,51 +187,66 @@ def main():
 
     # Load original ESM-2 model
     base_model_name = f"/g/data/gi52/jaime/esm2_{params}M_model"
-    base_tokenizer = EsmTokenizer.from_pretrained(base_model_name)
-    base_model = EsmForMaskedLM.from_pretrained(base_model_name)
+    tokenizer = EsmTokenizer.from_pretrained(base_model_name)
 
-    # Load missense fine-tuned model
-    ms_model_path = f"/g/data/gi52/jaime/trained/esm2_{params}M_model/missense/run10/epoch0_batch18000"
-    ms_tokenizer = EsmTokenizer.from_pretrained(ms_model_path)
-    ms_model = EsmForMaskedLM.from_pretrained(ms_model_path)
+    for batch_num in range(1000, 5001, 1000):
 
-    # Load base model separately for comparison
-    base_model_fresh = EsmForMaskedLM.from_pretrained(base_model_name)
+        print(f"Processing batch number: {batch_num}")
+        
+        base_model = EsmForMaskedLM.from_pretrained(base_model_name)
 
-    base_dir = f"/g/data/gi52/jaime/trained/esm2_{params}M_model/missense/run10/heatmaps_scaled/epoch0_batch18000"
-    os.makedirs(base_dir, exist_ok=True)
+        # Load missense fine-tuned model
+        ms_adapter_path = f"/g/data/gi52/jaime/trained/esm2_{params}M_model/missense/run10/epoch0_batch{batch_num}"
+        # Load the adapter into the model
+        ms_model = PeftModel.from_pretrained(base_model, ms_adapter_path, is_trainable=False )
 
-    print(f"Generating heatmaps for {gene} using ESM2-{params}M models...")
+        # Merge the adapter weights into the base model
+        csm_model = ms_model.merge_and_unload()
+        csm_model.eval()
 
-    # Generate heatmaps
-    base_heatmap, amino_acids = generate_heatmap(sequence, base_model_fresh, base_tokenizer)
-    ms_heatmap, _ = generate_heatmap(sequence, ms_model, ms_tokenizer)
+        print(csm_model)
 
-    # Compute difference
-    ms_diff_heatmap = ms_heatmap - base_heatmap
+        # Load base model separately for comparison
+        base_model_fresh = EsmForMaskedLM.from_pretrained(base_model_name)
 
-    plot_heatmap(params, gene, base_heatmap, "Original ESM2 Model (LLRs)", sequence, base_dir, amino_acids)
-    plot_heatmap(params, gene, ms_heatmap, "Fine-tuned Missense Model (LLRs)", sequence, base_dir, amino_acids)
-    plot_heatmap(params, gene, ms_diff_heatmap, "Difference (Fine-tuned Missense - Original)", sequence, base_dir, amino_acids)
+        base_dir = f"/g/data/gi52/jaime/trained/esm2_{params}M_model/missense/run10/heatmaps_scaled/{gene}"
+        os.makedirs(base_dir, exist_ok=True)
 
-   
-    # Generate heatmap with mutations as dots in positions
-    plot_heatmap(ms_diff_heatmap, gene, "Difference (Fine-tuned Missense - Original)", 
-                           sequence, base_dir, amino_acids)
-    plot_heatmap(ms_heatmap, gene, "Fine-tuned Missense Model", sequence, 
-                           base_dir, amino_acids)
+        print(f"Generating heatmaps for {gene} using ESM2-{params}M models...")
+
+        # Generate heatmaps
+        base_heatmap, amino_acids = generate_heatmap(sequence, base_model_fresh, tokenizer)
+        ms_heatmap, _ = generate_heatmap(sequence, csm_model, tokenizer)
+
+        # Compute difference
+        ms_diff_heatmap = ms_heatmap - base_heatmap
+
+        plot_heatmap(base_heatmap, gene, f"Epoch 0, Batch {batch_num}: Original ESM2 Model (LLRs)", sequence, base_dir, amino_acids)
+        plot_heatmap(ms_heatmap, gene, f"Epoch 0, Batch {batch_num}: Fine-tuned Missense Model (LLRs)", sequence, base_dir, amino_acids)
+        plot_heatmap(ms_diff_heatmap, gene, f"Epoch 0, Batch {batch_num}: Difference (Fine-tuned Missense - Original)", sequence, base_dir, amino_acids)
+
     
+        # Generate heatmap with scaling -1 to 1
+        scaled_plot_heatmap(base_heatmap, gene, f"Epoch 0, Batch {batch_num}: Scaled Original ESM2 Model (LLRs)", sequence, base_dir, amino_acids)
+        scaled_plot_heatmap(ms_heatmap, gene, f"Epoch 0, Batch {batch_num}: Scaled Fine-tuned Missense Model (LLRs)", sequence, base_dir, amino_acids)
+        scaled_plot_heatmap(ms_diff_heatmap, gene, f"Epoch 0, Batch {batch_num}: Scaled Difference (Fine-tuned Missense - Original)", sequence, base_dir, amino_acids)
+       
+        print(f"[Batch {batch_num}] Heatmaps generated and saved to {base_dir}")
 
-    # Compare amino acid predictions
-    masked_pos = 100
+        # Compare amino acid predictions
+        masked_pos = 245
 
-    original_preds = topk_predictions(base_model, base_tokenizer, sequence, masked_pos)
-    ms_preds = topk_predictions(ms_model, ms_tokenizer, sequence, masked_pos)
-    #fs_preds = topk_predictions(fs_model, fs_tokenizer, sequence, masked_pos)
+        original_preds = topk_predictions(base_model_fresh, tokenizer, sequence, masked_pos)
+        ms_preds = topk_predictions(csm_model, tokenizer, sequence, masked_pos)
+        #fs_preds = topk_predictions(fs_model, fs_tokenizer, sequence, masked_pos)
 
-    print(f"Original model top predictions at position {masked_pos}:", original_preds)
-    print(f"Fine-tuned missense model top predictions at position {masked_pos}:", ms_preds)
-    #print(f"Fine-tuned frameshift model top predictions at position {masked_pos}:", fs_preds)
+        print(f"Original model top predictions at position {masked_pos}:", original_preds)
+        print(f"Fine-tuned missense model top predictions at position {masked_pos}:", ms_preds)
+        #print(f"Fine-tuned frameshift model top predictions at position {masked_pos}:", fs_preds)
+
+        # delete models to free up memory
+        del csm_model, ms_model, base_model, base_model_fresh
+        torch.cuda.empty_cache()
 
 
 if __name__ == '__main__':
