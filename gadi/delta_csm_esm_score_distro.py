c4697697d77928592fcb3efbbf2054c2a9c1d9d3
Jaime Taitz <jtaitz@ccia.org.au>
2025-11-02 05:30:12 +0000

    added cartesian plot esm vs csm score

--- gadi/delta_csm_esm_score_distro.py
+++ gadi/delta_csm_esm_score_distro.py
@@ -17,6 +17,7 @@ from plotnine import ( ggplot, aes, geom_density, theme_minimal, scale_color_man
     scale_fill_manual)
 import os
 from sklearn.preprocessing import StandardScaler
+import numpy as np
 
 def compute_mut_score(model, tokenizer, mutation, sequence):
     model.eval()
@@ -247,14 +248,73 @@ def delta_extremity_mut_distro_plot(csm_data, save_dir, batch_num):
     p.save(save_path, width=8, height=5, dpi=300)
     print(f"Saved plotnine plot to {save_path}")
 
+def cartesian_plot(filtered_data, save_dir):
+    "Plot esm score versus csm score scatter plot"
+
+    csm_data = filtered_data.copy()  #
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)   
+
+    plt.figure(figsize=(8, 6))
+    ax = sns.scatterplot(
+        data=csm_data,
+        y="esm_score",
+        x="csm_score",
+        hue="clinvar_label",
+        alpha=0.7
+    )   
+    
+    # --- Set limits rounded to nearest 5 ---
+    min_x = np.floor(csm_data["csm_score"].min() / 5) * 5
+    min_y = np.floor(csm_data["esm_score"].min() / 5) * 5
+
+    # --- Set limits rounded to nearest 5 ---
+    max_x = np.ceil(csm_data["csm_score"].max() / 5) * 5
+    max_y = np.ceil(csm_data["esm_score"].max() / 5) * 5
+
+    # Invert both axes to make values decrease
+    ax.invert_xaxis()
+    
+
+    # --- Set ticks every 5 units ---
+    ax.set_xticks(np.arange(0, min_x - 5, -5))
+    ax.set_yticks(np.arange(0, min_y - 5, -5))
+
+    # --- Keep axes on left and top ---
+    ax.spines['left'].set_visible(True)
+    ax.spines['top'].set_visible(True)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+    # Move labels to left/top
+    ax.xaxis.set_label_position('top')
+    ax.xaxis.tick_top()
+    ax.yaxis.set_label_position('left')
+    ax.yaxis.tick_left()
+
+    # --- Add grid and labels ---
+    ax.grid(True, linestyle='--', alpha=0.3)
+    plt.title("TP53 Gene: CSM Score vs ESM Score by ClinVar Label")
+    plt.ylabel("ESM Score")
+    plt.xlabel("CSM Score")
+
+    sns.despine(left=False, top=False)
+    plt.tight_layout()
+
+    save_path = os.path.join(save_dir, f"tp53_esm_vs_csm_scatter_grid_filt.png")
+    plt.savefig(save_path, dpi=300, bbox_inches='tight')
+    plt.close()
+    print(f"Saved to {save_path}")
+
 def main():
     
     
     # Load dataset with ClinVar labels and mutations, and precomputed esm and csm scores 
-    tp53_clinvar = pd.read_csv("/g/data/gi52/jaime/clinvar/run11_ms/epoch0_batch10000_tp53_clinvar_csm_scores.csv")
+    tp53_clinvar = pd.read_csv("/g/data/gi52/jaime/clinvar/run11_ms/tp53/epoch0_batch10000_tp53_clinvar_csm_scores.csv")
 
     # save directory
-    save_dir = f"/g/data/gi52/jaime/clinvar/run11_ms"
+    save_dir = f"/g/data/gi52/jaime/clinvar/run11_ms/tp53"
     
     # rename columns for plotting
     df = tp53_clinvar.copy()
@@ -263,8 +323,12 @@ def main():
     )
 
     # filter out only Pathogenic, Benign, Uncertain Significance, and Conflicting classifications of pathogenicity
-    filtered_data = df[df['clinvar_label'].isin(['Pathogenic', 'Benign', 'Uncertain significance','Conflicting classifications of pathogenicity'])]
-        
+    filtered_data = df[df['clinvar_label'].isin([
+        'Pathogenic', 'Benign', 'Uncertain significance','Conflicting classifications of pathogenicity',
+        'Pathogenic/Likely pathogenic', 'Likely pathogenic', 'Benign/Likely benign', 'Likely benign'])]
+    
+    cartesian_plot(filtered_data, save_dir)
+
     delta_extremity_mut_distro_plot(filtered_data, save_dir, 10000)
     scaled_delta_extremity_mut_distro_plot(filtered_data, save_dir, 10000)
 
